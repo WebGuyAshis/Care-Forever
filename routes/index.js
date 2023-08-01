e8bf571638e81c073bd8f93c9f8d89c968f3147f
Ashis <developer.ashis99@gmail.com>
2023-08-01 23:34:35 +0530

    Added Health Reminder Page

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const homeController = require('../controllers/homeController')
-
+const healthContainer = require('../controllers/healthReminderPageController');
 
 
 
 
 
 router.get('/', homeController.home);
+router.use('/', healthContainer.home )
 
 module.exports = router;
